6ce176978296b0ba1d4d0a953a05c4c6dc60fbe0
Rajmohan <rajmohan5323@gmail.com>
2024-06-19 17:09:49 +0530

    Upto Madify and Delete

--- src/main/java/com/raj/deleterecordwithcaptcha/DeleteRecordWithValidateCaptchaExcel.java
+++ src/main/java/com/raj/deleterecordwithcaptcha/DeleteRecordWithValidateCaptchaExcel.java
@@ -45,13 +45,13 @@ public class DeleteRecordWithValidateCaptchaExcel {
         driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
         driver.get("https://tn.unitetools.in/");
 
-        driver.findElement(By.id("user")).sendKeys("TN12101860_de@coopsindia.com");
+        driver.findElement(By.id("user")).sendKeys("TN12102253_de@coopsindia.com");
         driver.findElement(By.id("pwd")).sendKeys("Unite@123");
         driver.findElement(By.id("btnvalidatelogin")).click();
 
         // Click on 'Customer Data' link
-        WebElement excelUploadLink = driver.findElement(By.linkText("Customer Data"));
-        excelUploadLink.click();
+        WebElement customerData = driver.findElement(By.linkText("Customer Data"));
+        customerData.click();
 
         // Navigate to the delete page
         WebElement deletePage = driver.findElement(By.xpath("//a[contains(@href,'/Utilities/DeleteCustomerData/DeleteCustomerData')]/li[contains(@class,'list-group-item') and contains(@class,'bg-primary')]"));
@@ -75,6 +75,7 @@ public class DeleteRecordWithValidateCaptchaExcel {
                 inputField.sendKeys(formattedValue); // Enter the value from Excel
 
                 WebDriverWait wait = new WebDriverWait(driver, 1);
+                
 
                 // Click on search button
                 WebElement searchButton = driver.findElement(By.id("iconMemberSearch"));
@@ -122,7 +123,111 @@ public class DeleteRecordWithValidateCaptchaExcel {
                         if (alertTitle1.isDisplayed()) {
                             WebElement okButton = alertBox1.findElement(By.xpath(".//button[@class='confirm']"));
                             okButton.click();
-                            continue;// Move to next iteration
+                          //  continue;// Move to next iteration
+                            // Added by rajmohan
+                             
+                          //  WebElement homeButton = driver.findElement(By.cssSelector("a.pl-2.pr-2.home-botton.bg-white"));
+                           // Click the element
+                          //  homeButton.click();
+                            
+                            
+                            WebElement homeButton = driver.findElement(By.xpath("//a[@href='/Home/MenuScreen']"));
+                            homeButton.click();
+                            
+                            // Locate and click the "Personal Details" tab
+                            WebElement personalDetailsTab = wait.until(ExpectedConditions.elementToBeClickable(By.id("v-pills-01-tab")));
+                            personalDetailsTab.click();
+                            
+                            wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("v-pills-01")));
+                             
+                             
+                            WebElement verifyButton = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#v-pills-01 .list-group a[href='/Membership/ModifyCustomerPersonalDeatils/VerifyorEditCustomerPersonalDeatils?formid=1003&moduleid=1']")));
+                            verifyButton.click();
+                            
+                            // Locate the input field by its ID and enter the value
+                            WebElement inputField1 = driver.findElement(By.id("idamissionno"));
+                            inputField1.clear(); // Clear the input field
+                            inputField1.sendKeys(formattedValue); // Enter the value from Excel  
+                            
+                             WebElement statusDropdown = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("Status")));
+                            Select statusSelect = new Select(statusDropdown);
+                            statusSelect.selectByVisibleText("Not Matched");
+                            
+                            WebElement reasonDropdown = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("ModificationReasonId")));
+                            Select reasonSelect = new Select(reasonDropdown);
+                            reasonSelect.selectByVisibleText("Record mismatch");
+                            
+                            WebElement editButton = wait.until(ExpectedConditions.elementToBeClickable(By.id("btnModify")));
+                            editButton.click();
+                            
+                            WebElement modifyButton = wait.until(ExpectedConditions.elementToBeClickable(By.id("btnModify")));
+                            modifyButton.click();
+                            
+                            WebElement confirmButton = wait.until(ExpectedConditions.elementToBeClickable(By.id("btnSavedataFRompopup")));
+                            confirmButton.click();
+                            
+                             WebElement alertBox2 = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("sweet-alert")));
+                             WebElement alertTitle2 = alertBox2.findElement(By.xpath(".//h2[text()='Data updated successfully']"));
+                         
+                             if (alertTitle2.isDisplayed()) {
+                             WebElement okButton2 = alertBox2.findElement(By.xpath(".//button[@class='confirm']"));
+                             okButton2.click();
+                             
+                            WebElement homeButton2 = driver.findElement(By.xpath("//a[@href='/Home/MenuScreen']"));
+                            homeButton2.click();
+                            
+                            WebElement customerData1 = driver.findElement(By.linkText("Customer Data"));
+                            customerData1.click();
+
+                            // Navigate to the delete page
+                            WebElement deletePage2 = driver.findElement(By.xpath("//a[contains(@href,'/Utilities/DeleteCustomerData/DeleteCustomerData')]/li[contains(@class,'list-group-item') and contains(@class,'bg-primary')]"));
+                            deletePage2.click();
+                             
+                            WebElement inputField2 = driver.findElement(By.id("idamissionno"));
+                            inputField2.clear(); // Clear the input field
+                            inputField2.sendKeys(formattedValue); // Enter the value from Excel
+                            
+                            WebElement searchButton1 = driver.findElement(By.id("iconMemberSearch"));
+                            searchButton1.click();
+                                try {
+                                    // Check if the "No Data Found" alert is present
+                                    WebElement alertBox3 = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("sweet-alert")));
+                                    WebElement alertTitle3 = alertBox3.findElement(By.xpath(".//h2[text()='No Data Found']"));
+
+                                    if (alertTitle3.isDisplayed()) {
+                                        WebElement okButton1 = alertBox3.findElement(By.xpath(".//button[@class='confirm']"));
+                                        okButton1.click();
+                                        continue;// Move to next iteration
+                                    }
+                                } catch (Exception e) {
+                                    // No alert found, proceed with deletion process
+                                    System.out.println("No alert found, proceeding with deletion for value: " + e.getMessage());
+                                }
+                                
+                                // Explicitly wait for the product dropdown to be clickable
+                                 WebElement productDropdown1 = wait.until(ExpectedConditions.elementToBeClickable(By.id("ProductCode")));
+                                 Select productSelect1 = new Select(productDropdown1);
+                                 productSelect1.selectByVisibleText("Customer");
+                                
+                                // WebDriverWait wait1 = new WebDriverWait(driver, 1);
+                                // Select "Admission No." from the task type dropdown
+                                Select taskTypeDropdown1 = new Select(driver.findElement(By.id("TaskType")));
+                                taskTypeDropdown1.selectByValue("1");
+
+                                wait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector("div.bg[style*='position: absolute']")));
+
+                                // Click the delete button
+                                WebElement deleteButton1 = driver.findElement(By.id("btnDelete"));
+                             //   wait.until(ExpectedConditions.elementToBeClickable(deleteButton));
+                              //  deleteButton.click();
+                                wait.until(ExpectedConditions.presenceOfElementLocated(By.id("btnDelete")));
+
+                               JavascriptExecutor js1 = (JavascriptExecutor) driver;
+                               js1.executeScript("arguments[0].click();", deleteButton1);
+                             //  continue;
+                             }
+                            
+                            
                         }
                     } catch (Exception e) {
                         // No alert found, proceed with deletion process
@@ -130,8 +235,8 @@ public class DeleteRecordWithValidateCaptchaExcel {
                     }
                     
                      // Find and read the captcha image
-                    WebElement captchaImage = driver.findElement(By.id("imgcapt"));
-                    String captchaText = readCaptcha(captchaImage);
+                     WebElement captchaImage = driver.findElement(By.id("imgcapt"));
+                     String captchaText = readCaptcha(captchaImage);
 
                     System.out.println("Captcha Text: " + captchaText);
                     
@@ -145,8 +250,8 @@ public class DeleteRecordWithValidateCaptchaExcel {
                     
 
                     // Handle SweetAlert confirmation dialog
-                    WebElement sweetAlert = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("sweet-alert")));
-                    WebElement h2Element = sweetAlert.findElement(By.tagName("h2"));
+                     WebElement sweetAlert = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("sweet-alert")));
+                     WebElement h2Element = sweetAlert.findElement(By.tagName("h2"));
                     String message = h2Element.getText().trim();
                     System.out.println("messageeeee--------> "+ message);
                     
@@ -160,7 +265,7 @@ public class DeleteRecordWithValidateCaptchaExcel {
                         } else if (message.equals("Enter valid captcha")) {
                             // Click OK button
                            
-                            WebElement okButton = sweetAlert.findElement(By.className("confirm"));
+                             WebElement okButton = sweetAlert.findElement(By.className("confirm"));
                             okButton.click();
                             
                             // Click Refresh Captcha button
@@ -168,7 +273,7 @@ public class DeleteRecordWithValidateCaptchaExcel {
                             refreshCaptchaButton.click();
                            
                             // Find and read the new captcha image
-                            WebElement captchaImage2 = driver.findElement(By.id("imgcapt"));
+                             WebElement captchaImage2 = driver.findElement(By.id("imgcapt"));
                             String captchaText2 = readCaptcha(captchaImage2);
 
                             System.out.println("Captcha Text 2: " + captchaText2);
